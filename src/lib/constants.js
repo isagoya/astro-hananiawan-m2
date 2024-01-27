c255af10e7dc15ae6e8783a4f36d0bd986e74f52
isagoya <isagoya39@gmail.com>
2024-01-27 12:38:09 +0900

    Meta.astro を追加

---
+++ src/lib/constants.js
@@ -0,0 +1,11 @@
+export const siteMeta = {
+	siteTitle: 'Hana ni Awan',
+	siteDesc: "This book is an inspiring account of a Hansen's disease survivor who overcame the harsh and cruel societal prejudice. It also serves as a record of a love anthem shared by many neighbors who supported him and lived alongside him.",
+	siteUrl: 'https://nananiawan.rainbow-culion.net',
+	siteLocale: 'en_US',
+	siteType: 'website',
+	siteIcon: '/favicon.ico',
+	siteImg: '/assets/images/hananiawan-cover1.jpg',
+};
+
+
